58bf78012e12935bf47c9ce551a5a2817f30fc4f
kiiooxx <58679302+kiiooxx@users.noreply.github.com>
2024-07-13 14:41:59 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 52 ms, Memory: 5440 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+public class Solution
+{
+    public static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+        
+        Console.Write(input.Length);
+    }
+}
